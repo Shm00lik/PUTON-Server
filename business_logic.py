2ecc7f4d6806cccf306cbd26bdfc88adcfe49c89
Shm00lik <70761433+Shm00lik@users.noreply.github.com>
2023-12-08 01:18:40 +0200

    Started implementing db in business logic but I have a but🥺

--- business_logic.py
+++ business_logic.py
@@ -1,10 +1,19 @@
 from httpLib.client import Client
 from httpLib.protocol import Request, Response
 from config import Config
+from typing import Callable
+import json
+from sqliteLib.database import Database
+from sqliteLib.table import Table
 import time
 
 
 class BusinessLogic:
+    print("Initializing database")
+    db = Database(Config.DATABASE_PATH)
+
+    usersTable = Table("users")
+
     @staticmethod
     def handleClient(request: Request, client: Client):
         print("Client connected from", client.clientAddress)
@@ -35,13 +44,28 @@ class BusinessLogic:
         client.send(response)
 
     @staticmethod
+    def validateRequest(request: Request, function: Callable[[Request], bool]) -> bool:
+        return function(request)
+
+    @staticmethod
     def register(request: Request) -> Response:
+        if not BusinessLogic.validateRequest(request, lambda r: r.payload is not None):
+            return Response(
+                content=json.dumps({"error": "Invalid request"}),
+                statusCode=Response.StatusCode.BAD_REQUEST,
+            )
+
+        BusinessLogic.usersTable.insert(
+            username=request.payload["username"],
+            password=request.payload["password"],
+        ).execute()
+
         print("Registering new user " + str(request.payload))
 
-        return Response(content="{'a': 'User registered successfully'}")
+        return Response(content="{'msg': 'User registered successfully'}")
 
     @staticmethod
     def login(request: Request) -> Response:
         print("Logging in user " + str(request))
 
-        return Response(content="'a': 'User logged in successfully'")
+        return Response(content="'msg': 'User logged in successfully'")
--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Config:
     HOST: str = "localhost"
-    PORT: int = 8080
-    DATABASE_PATH: str = "database/database.sqlite"
+    PORT: int = 3339
+    DATABASE_PATH: str = "./database/database.sqlite"
     SOCKET_TIMEOUT: int = 1
 
     CTF_FLAG: str = "CTF{wh4t_4_w0nd3rful_w0r1d!}"
